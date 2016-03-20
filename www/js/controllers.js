e7712e43ac652892191e50644a28c0fe5bce8617
derekhua <derekchristopherhua@gmail.com>
2016-03-20 16:18:06 -0400

    first commit

---
+++ www/js/controllers.js
@@ -0,0 +1,38 @@
+angular.module('app.controllers', [])
+  
+.controller('dashboardCtrl', function($scope) {
+
+})
+   
+.controller('scheduleCtrl', function($scope) {
+
+})
+   
+.controller('workoutCreationCtrl', function($scope) {
+
+})
+      
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('nutritionCtrl', function($scope) {
+
+})
+   
+.controller('visualizationCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('exerciseSearchCtrl', function($scope) {
+
+})
+   
+.controller('workoutsCtrl', function($scope) {
+
+})
+ 
